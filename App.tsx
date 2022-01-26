e4cff9097ab0af01048e7bfeb226db3f2f077b9d
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-26 14:05:04 -0300

    adicionando stilos globais

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
+import theme from './src/Global/Styles/theme';
 import { Dashboard } from './src/Screens/Dashboard';
 
 export default function App() {
   return (
-    <Dashboard />
+    <ThemeProvider  theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
   );
 }
 
