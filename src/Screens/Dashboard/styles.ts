9ff448e5e9fe096bb51614fbdec98ec79e646779
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-27 16:50:38 -0300

    Adicionando propriedades no styled components

--- src/Screens/Dashboard/styles.ts
+++ src/Screens/Dashboard/styles.ts
@@ -55,4 +55,8 @@ export const Icon = styled(Feather)`
     color: ${({theme }) => theme.colors.secondary};
     font-size: ${RFValue(24)}px;
 `
-
+export const HighlightCards = styled.ScrollView.attrs({
+    horizontal: true,
+    showsHorizontalScrollIndicator: true,
+    contentContainerStyle: { paddingHorizontal: 24}
+})``;
