37d0992e1818d447deebc886c74cf98f7cbb6d13
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-29 12:27:11 -0300

    Finalizando Modal

--- src/Screens/CategorySelect/styles.ts
+++ src/Screens/CategorySelect/styles.ts
@@ -1,5 +1,7 @@
-import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+
+import { RFValue } from "react-native-responsive-fontsize";
+import { Feather } from '@expo/vector-icons'
 
 export const Container = styled.View`
     flex: 1;
@@ -20,5 +22,31 @@ export const Header = styled.View`
 export const Title = styled.Text`
     font-family: ${({ theme }) => theme.fonts.regular};
     color: ${({ theme }) => theme.colors.shape};
-    font-size:  ${RFValue(18)}px;;
+    font-size:  ${RFValue(18)}px;
+`
+export const Category = styled.View`
+    width: 100%;
+    padding: ${RFValue(15)}px;
+
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const Icon = styled(Feather)`
+    font-size:  ${RFValue(20)}px;
+    margin-right: 16px;
+`
+export const Name = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-size:  ${RFValue(14)}px;
+`;
+export const Separator = styled.View`
+    height: 1px;
+    width: 100%;
+    background-color: ${({ theme }) => theme.colors.text};
+`
+
+export const Footer = styled.View`
+    width: 100%;
+    padding: 24px;
 `
