40c73dd13f418b51c60b2459c19862735793a811
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-30 13:44:45 -0300

    Adicionando Navegação

--- App.tsx
+++ App.tsx
@@ -10,8 +10,9 @@ import {
 import AppLoading from 'expo-app-loading';
 
 import theme from './src/Global/Styles/theme';
-import { Dashboard } from './src/Screens/Dashboard';
-import { Register } from './src/Screens/Register';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider  theme={theme}>
-      <Register />
+      <NavigationContainer>
+       <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
