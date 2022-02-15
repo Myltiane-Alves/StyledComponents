a208dd1ae5ba7f42cc61226a571e7e1ad5daf552
Myltiane Alves <myltiane.aux@gmail.com>
2022-02-15 10:10:12 -0300

    Adicionando login social

--- src/Screens/Signin/index.tsx
+++ src/Screens/Signin/index.tsx
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import { Alert } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import GoogleSvg from '../../assets/logo.svg';
 import AppleSvg from '../../assets/logo.svg';
 import LogoSvg from '../../assets/logo.svg';
+
 import { useAuth } from '../../hooks/auth';
 
 import { SignInSocialButton } from '../../components/SignInSocialButton';
@@ -18,8 +20,17 @@ import {
     FooterWrapper,
 } from './styles';
 export function SignIn() {
-    const { user } = useAuth();
+    const { signInWithGoogle } = useAuth();
 
+    async function handleSignInWithGoogle() {
+        try {
+            await signInWithGoogle();
+
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Não foi possível conectar a conta Google');
+        }
+    }
     return (
         <Container>
             <Header>
@@ -45,6 +56,7 @@ export function SignIn() {
                     <SignInSocialButton
                         title="Entrar com Google"
                         svg={GoogleSvg}    
+                        onPress={handleSignInWithGoogle}
                     />
 
                     <SignInSocialButton
